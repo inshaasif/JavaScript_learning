791b90601b5da8b9d99e2e223d0bb4814a4fc888
inshaasif <inshaasif19@gmail.com>
2024-01-12 10:59:16 +0500

    updated

---
+++ Learning/asset/js/external.js
@@ -0,0 +1,11 @@
+function demoTextChange() {
+    document.getElementById("demo").innerHTML = "this is JS.";
+}
+
+function bulbOn() {
+    document.getElementById('myImage').src='./asset/images/bulbon.webp'
+}
+
+function bulbOff() {
+    document.getElementById('myImage').src='./asset/images/bulboff.jpg'
+}
